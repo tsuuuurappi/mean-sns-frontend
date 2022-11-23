63d322b94cc5642a9c60a2853e732cf24fea8991
iamtsukanoshin <iamtsukanoshin@gmail.com>
2022-11-24 08:45:50 +0900

    add dummyData

---
+++ src/components/post/Post.jsx
@@ -0,0 +1,38 @@
+import { MoreVert } from '@mui/icons-material';
+import React from 'react';
+import './Post.css';
+import { Users } from '../../dummyData';
+
+export default function Post({ post }) {
+  // const user = Users.filter((user) => user.id === 1);
+  // console.log(user[0].username);
+  return (
+    <div className="post">
+      <div className="postWrapper">
+        <div className="postTop">
+          <div className="postTopLeft">
+            <img src={Users.filter((user) => user.id === post.id)[0].profilePicture} alt="" className="postProfileImg" />
+            <span className="postUsername">{Users.filter((user) => user.id === post.id)[0].username}</span>
+            <span className="postDate">{post.date}</span>
+          </div>
+          <div className="postTopRight">
+            <MoreVert />
+          </div>
+        </div>
+        <div className="postCenter">
+          <span className="postText">{post.desc}</span>
+          <img src={post.photo} alt="" className="postImg" />
+        </div>
+        <div className="postBottom">
+          <div className="postBottomLeft">
+            <img src="./assets/heart.png" alt="" className="likeIcon" />
+            <span className="postLikeCounter">{post.like}がいいねを押しました</span>
+          </div>
+          <div className="postBottomRight">
+            <span className="postCommentText">{post.comment}:コメント</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import './Home.css';
+import Rightbar from '../../components/rightbar/Rightbar';
+import Sidebar from '../../components/sidebar/Sidebar';
 import Topbar from '../../components/topbar/Topbar';
+import Timeline from '../../components/timeline/TimeLine';
 
 export default function Home() {
   return (
     <>
       <Topbar />
-      {/* sidebar */}
-      {/* timeline */}
-      {/* rightbar */}
+      <div className="homeContainer">
+        <Sidebar />
+        <Timeline />
+        <Rightbar />
+      </div>
     </>
   );
 }
