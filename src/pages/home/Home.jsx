8a6ba7ce8955ffe73fc8f49ab2176ac164dc4aac
iamtsukanoshin <iamtsukanoshin@gmail.com>
2022-11-23 21:05:49 +0900

    add top bar

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Topbar from '../../components/topbar/Topbar';
+
+export default function Home() {
+  return (
+    <>
+      <Topbar />
+      {/* sidebar */}
+      {/* timeline */}
+      {/* rightbar */}
+    </>
+  );
+}
