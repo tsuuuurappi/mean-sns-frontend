2c80347d128ebebbc9fb1a88b95f30f1c859891e
iamtsukanoshin <iamtsukanoshin@gmail.com>
2022-11-24 12:33:09 +0900

    add like-func

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,6 +1,8 @@
 import { Bookmark, Home, MessageRounded, Notifications, Person, Search, Settings } from '@mui/icons-material';
 import React from 'react';
+import CloseFriend from '../closeFriend/CloseFriend';
 import './Sidebar.css';
+import { Users } from '../../dummyData';
 
 export default function Sidebar() {
   return (
@@ -38,18 +40,9 @@ export default function Sidebar() {
         </ul>
         <hr className="sidebarHr" />
         <ul className="sidebarFriendList">
-          <li className="sidebarFriend">
-            <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
-            <span className="sidebarFriendName">Shin Code</span>
-          </li>
-          <li className="sidebarFriend">
-            <img src="/assets/person/3.jpeg" alt="" className="sidebarFriendImg" />
-            <span className="sidebarFriendName">Tanaka</span>
-          </li>
-          <li className="sidebarFriend">
-            <img src="/assets/person/4.jpeg" alt="" className="sidebarFriendImg" />
-            <span className="sidebarFriendName">Sato</span>
-          </li>
+          {Users.map((user) => (
+            <CloseFriend user={user} key={user.id} />
+          ))}
         </ul>
       </div>
     </div>
